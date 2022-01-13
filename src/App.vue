3ed57797c3b8fa2d2a0e5c29750b5fc490340106
Nurettin-Frontend <noureibo7@gmail.com>
2022-01-13 04:36:12 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,12 @@
   <v-app id="app" class="toggle-main-bg">
     dsdasdsa
     <router-view />
-    <!-- <cursor-fx :config="config"  :hide-inside="true"> </cursor-fx> -->
   </v-app>
 </template>
 
 <script>
 export default {
   data: () => ({
-    config: {
-      lerps: {
-        dot: 4,
-        circle: 0.5,
-        custom: 0.8,
-      },
-      scale: {
-        ratio: 0.18,
-        min: 0.5,
-        max: 1,
-      },
-      opacity: 0.1,
-    },
   }),
 };
 </script>
